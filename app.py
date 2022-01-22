ac8623b97542247fd4ec2cedd4014dc68a787b11
meganelisabethfinch <meganelisabethfinch@gmail.com>
2022-01-22 14:43:08 +0000

    Flask template 2

---
+++ app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+
+DEVELOPMENT_ENV  = True
+
+app = Flask(__name__)
+
+app_data = {
+    "name":         "Peter's Starter Template for a Flask Web App",
+    "description":  "A basic Flask app using bootstrap for layout",
+    "author":       "Peter Simeth",
+    "html_title":   "Peter's Starter Template for a Flask Web App",
+    "project_name": "Starter Template",
+    "keywords":     "flask, webapp, template, basic"
+}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', app_data=app_data)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', app_data=app_data)
+
+
+@app.route('/service')
+def service():
+    return render_template('service.html', app_data=app_data)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', app_data=app_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=DEVELOPMENT_ENV)
